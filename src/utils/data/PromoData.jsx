59b5a6dec47b1efb28c81e18eeec66209f8136ac
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-10-08 14:33:43 +0600

    Add promotional data for discounts and offers

---
+++ src/utils/data/PromoData.jsx
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        title: "50% Off on First Order",
+        description: "Get 50% off on your first order with code FIRST50",
+        image: "https://pathao.com/wp-content/uploads/2025/08/Ride-Dine-Save-15-Cashback-with-City-Bank-American-Express-Credit-Cards.png",
+    },
+    {
+        id: 2,
+        title: "Free Delivery",
+        description: "Enjoy free delivery on orders above $20",
+        image: "https://pathao.com/wp-content/uploads/2025/03/Savor-More-Win-More-with-Pathao-Food.jpeg",
+    },
+    {
+        id: 3,
+        title: "Buy One Get One Free",
+        description: "Order now and get another item for free",
+        image: "https://pathao.com/wp-content/uploads/2024/06/50-Cashback-on-Pathao-Food-using-Pathao-Pay-768x402.png",
+    },
+    {
+        id: 4,
+        title: "20% Off on All Items",
+        description: "Use code SAVE20 to get 20% off on all items",
+        image: "https://pathao.com/wp-content/uploads/2024/06/Up-to-300TK-Cashback-at-Selected-Restaurants-with-Pathao-Pay-768x402.png",
+    },
+]
