989d0e66258dd0ad483d83a7389a88eefd9593c0
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-10-09 23:12:01 +0600

    Add CommonStyles for consistent styling across components

---
+++ src/utils/styles/CommonStyle.jsx
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native";
+import { Colors } from "../constants/Color";
+
+export const CommonStyles = StyleSheet.create({
+  title_16_bold: {
+    fontSize: 16,
+    fontWeight: "700",
+    color: Colors.dark,
+  },
+  text_12_regular: {
+    fontSize: 12,
+    fontWeight: "400",
+    color: Colors.gray_2,
+  },
+  text_14_regular: {
+    fontSize: 14,
+    color: Colors.gray_2,
+  },
+
+  button_text_12: {
+    color: Colors.primary_2,
+    fontSize: 12,
+    fontWeight: "600",
+  },
+
+
+  
+});
