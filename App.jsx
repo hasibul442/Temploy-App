b5b1e063b19d006dfee1f9f5c25802ca45a766cb
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-11-25 16:04:10 +0600

    Wrap NavigationContainer in PaperProvider

--- App.jsx
+++ App.jsx
@@ -1,14 +1,16 @@
-import { NavigationContainer } from '@react-navigation/native';
-import Navigator from './src/components/navigation/Navigatior';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from "@react-navigation/native";
+import Navigator from "./src/components/navigation/Navigatior";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { PaperProvider } from "react-native-paper";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Navigator />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <PaperProvider>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Navigator />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </PaperProvider>
   );
 }
-
