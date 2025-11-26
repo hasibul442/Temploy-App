82c96e1aff599ec1e96d928284ecb8b1d5b48052
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-11-26 21:07:15 +0600

    Refactor App component structure to include AlertNotificationRoot for improved notification handling

--- App.jsx
+++ App.jsx
@@ -2,15 +2,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import Navigator from "./src/components/navigation/Navigatior";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { PaperProvider } from "react-native-paper";
+import { AlertNotificationRoot } from "react-native-alert-notification";
 
 export default function App() {
   return (
-    <PaperProvider>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <Navigator />
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </PaperProvider>
+    <SafeAreaProvider>
+        <PaperProvider>
+      <AlertNotificationRoot>
+          <NavigationContainer>
+            <Navigator />
+          </NavigationContainer>
+      </AlertNotificationRoot>
+        </PaperProvider>
+    </SafeAreaProvider>
   );
 }
