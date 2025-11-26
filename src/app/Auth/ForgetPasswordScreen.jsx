175b0b38e493e94b8c608040d643976d4b804313
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-11-26 15:19:00 +0600

    Implement Forget Password Screen component

---
+++ src/app/Auth/ForgetPasswordScreen.jsx
@@ -0,0 +1,167 @@
+import { useNavigation } from "@react-navigation/native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+  ScrollView,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { AntDesign } from "@expo/vector-icons";
+import { Colors } from "../../utils/constants/Color";
+import { CommonStyles } from "../../utils/styles/CommonStyle";
+import { ButtonStyle } from "../../utils/styles/ButtonStyle";
+import { TextInput } from "react-native-paper";
+
+const { width } = Dimensions.get("window");
+
+function ForgetPasswordScreen() {
+  const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+
+  const handleSubmitButton = () => {
+    navigation.navigate("Auth", { screen: "Login" });
+  };
+
+  return (
+    <SafeAreaView style={CommonStyles.safeArea}>
+      <View style={styles.backgroundSolid}>
+        <View style={CommonStyles.circleTopLeft} />
+        <View style={CommonStyles.circleTopRight} />
+        <View style={CommonStyles.circleBottomRight} />
+
+        <ScrollView
+          contentContainerStyle={styles.scrollContent}
+          keyboardShouldPersistTaps="handled"
+        >
+          {/* Logo Section */}
+          <View style={styles.logoContainer}>
+            {/* <Image
+              source={require("../assets/logo/logo-1.png")}
+              style={styles.dumbbellIcon}
+            /> */}
+            {/* <Text style={styles.logoText}>Temploy</Text> */}
+          </View>
+
+          <Text style={styles.title}>Forget Password</Text>
+          <Text style={styles.subtitle}>
+            Please enter your email address to reset your password.
+          </Text>
+
+          <TextInput
+            mode="flat"
+            label="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+            textContentType="emailAddress"
+            placeholder="Please Insert Email"
+            value={email}
+            onChangeText={setEmail}
+            outlineColor="#333"
+            activeOutlineColor={Colors.success}
+            textColor="#000"
+            style={{
+              backgroundColor: "white",
+              width: "100%",
+              marginBottom: 20,
+            }}
+            theme={{ colors: { background: "white" } }}
+          />
+
+          <TouchableOpacity
+            style={ButtonStyle.signInButton_2}
+            onPress={handleSubmitButton}
+          >
+            <Text style={ButtonStyle.signInButtonText_2}>Continue</Text>
+          </TouchableOpacity>
+
+          <View style={styles.signUpContainer}>
+            <Text style={styles.signUpText}>
+              Don't remember your email? Contact us at{" "}
+              <TouchableOpacity>
+                <Text style={styles.signUpLink}>help@temploy.com</Text>
+              </TouchableOpacity>
+            </Text>
+          </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  backgroundSolid: {
+    flex: 1,
+    position: "relative",
+  },
+  scrollContent: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 30,
+    paddingVertical: 50,
+  },
+
+  // --- Logo Section ---
+  logoContainer: {
+    alignItems: "center",
+    marginBottom: 50,
+  },
+  dumbbellIcon: {
+    width: 150,
+    height: 80,
+    resizeMode: "contain",
+    tintColor: Colors.white,
+    marginBottom: 10,
+  },
+  logoText: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: Colors.white,
+    letterSpacing: 2,
+  },
+
+  // --- Welcome Text ---
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: Colors.white,
+    marginBottom: 20,
+  },
+
+  subtitle: {
+    fontSize: 16,
+    color: Colors.white,
+    marginBottom: 30,
+    textAlign: "center",
+  },
+
+  signUpContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 8,
+    flexWrap: "wrap",
+  },
+  signUpText: {
+    fontSize: 13.5,
+    color: Colors.white,
+    letterSpacing: 0.2,
+    textAlign: "center",
+  },
+  signUpLink: {
+    fontSize: 13.5,
+    color: Colors.white,
+    textDecorationLine: "underline",
+    textDecorationColor: Colors.white,
+    fontWeight: "bold",
+    letterSpacing: 0.2,
+  },
+});
+
+export default ForgetPasswordScreen;
