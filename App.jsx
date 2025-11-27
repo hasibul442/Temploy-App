c3722ce13293d45a8b46c152a3500e759ea4b366
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-11-27 12:47:26 +0600

    Refactor App component to conditionally use AlertNotificationRoot

--- App.jsx
+++ App.jsx
@@ -3,17 +3,27 @@ import Navigator from "./src/components/navigation/Navigatior";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { PaperProvider } from "react-native-paper";
 import { AlertNotificationRoot } from "react-native-alert-notification";
+import { Platform } from "react-native";
 
 export default function App() {
-  return (
+  const AppContent = (
     <SafeAreaProvider>
-        <PaperProvider>
-      <AlertNotificationRoot>
-          <NavigationContainer>
-            <Navigator />
-          </NavigationContainer>
-      </AlertNotificationRoot>
-        </PaperProvider>
+      <PaperProvider>
+        <NavigationContainer>
+          <Navigator />
+        </NavigationContainer>
+      </PaperProvider>
     </SafeAreaProvider>
+  );
+
+  // Only use AlertNotificationRoot on native platforms (iOS/Android)
+  if (Platform.OS === 'web') {
+    return AppContent;
+  }
+
+  return (
+    <AlertNotificationRoot>
+      {AppContent}
+    </AlertNotificationRoot>
   );
 }
