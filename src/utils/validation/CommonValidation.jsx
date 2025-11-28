a3ecef62312be065445533ffeead36501b60af63
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-11-28 22:12:14 +0600

    Fix equality checks in CommonValidation function

--- src/utils/validation/CommonValidation.jsx
+++ src/utils/validation/CommonValidation.jsx
@@ -1,16 +1,16 @@
 export function CommonValidation(values) {
-  if (values.fname=="")
+  if (values.fname == "")
     return { field: "fname", message: "First name is required" };
 
-  if (values.lname=="")
+  if (values.lname == "")
     return { field: "lname", message: "Last name is required" };
 
-  if (values.email=="") return { field: "email", message: "Email is required" };
+  if (values.email == "") return { field: "email", message: "Email is required" };
 
   if (values.email && !/\S+@\S+\.\S+/.test(values.email))
     return { field: "email", message: "Enter a valid email" };
 
-  if (values.password=="")
+  if (values.password == "")
     return { field: "password", message: "Password is required" };
 
   if (values.password.length < 6)
