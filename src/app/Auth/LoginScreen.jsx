b795578f9f50fd8c69ef386864efde66e9b5ba0d
Hasibul Hasan <hasibulhasan442@gmail.com>
2025-12-11 21:09:22 +0600

    Enhance LoginScreen: Replace alerts with Toast messages for validation and login errors, add HelperText for input errors

--- src/app/Auth/LoginScreen.jsx
+++ src/app/Auth/LoginScreen.jsx
@@ -7,9 +7,10 @@ import {
   TouchableOpacity,
   Dimensions,
   Alert,
+  ToastAndroid,
 } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { TextInput } from "react-native-paper";
+import { HelperText, TextInput } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Colors } from "../../utils/constants/Color";
 import { CommonStyles } from "../../utils/styles/CommonStyle";
@@ -17,8 +18,8 @@ import { ButtonStyle } from "../../utils/styles/ButtonStyle";
 import { ErrorAlert } from "../../utils/helper/AlertHelper";
 import { CommonValidation } from "../../utils/validation/CommonValidation";
 import { postData } from "../../utils/helper/HttpHelper";
-import { useDispatch } from 'react-redux';
-import { loginUser } from '../../slices/authSlice';
+import { useDispatch } from "react-redux";
+import { loginUser } from "../../slices/authSlice";
 
 const { width, height } = Dimensions.get("window");
 
@@ -31,7 +32,7 @@ function LoginScreen() {
   const navigation = useNavigation();
   const dispatch = useDispatch();
 
-  const handleSignIn = async() => {
+  const handleSignIn = async () => {
     const validationError = CommonValidation({
       email,
       password,
@@ -39,31 +40,39 @@ function LoginScreen() {
 
     if (validationError) {
       setError(validationError);
-      ErrorAlert(validationError.message);
-      Alert.alert("Validation Error", validationError.message);
+      ToastAndroid.show(validationError.message, ToastAndroid.SHORT);
       return;
     }
     setError({ field: "", message: "" });
 
-    try{
-      await postData("/api/v1/auth/user/login", {
-        email,
-        password,
-      }, false).then(async (response)=>{
-        if(response?.status == 200){
-          await dispatch(loginUser({
-            token: response?.token?.token,
-            user: response?.user || null
-          })).unwrap();
-        }else{
-          ErrorAlert(response?.message || "An error occurred during login.");
-          Alert.alert("Login Failed", response?.message || "An error occurred during login.");
+    try {
+      await postData(
+        "/api/v1/auth/user/login",
+        {
+          email,
+          password,
+        },
+        false
+      ).then(async (response) => {
+        if (response?.status == 200) {
+          await dispatch(
+            loginUser({
+              token: response?.token?.token,
+              user: response?.user || null,
+            })
+          ).unwrap();
+        } else {
+          ToastAndroid.show(
+            response?.message || "Login failed. Please try again.",
+            ToastAndroid.SHORT
+          );
         }
       });
     } catch (error) {
-      ErrorAlert("An error occurred during login. Please try again.");
-      console.error("Login error:", error);
-      Alert.alert("Error", "An error occurred during login. Please try again.");
+      ToastAndroid.show(
+        "An error occurred during login. Please try again.",
+        ToastAndroid.SHORT
+      );
     }
   };
 
@@ -99,12 +108,19 @@ function LoginScreen() {
                   autoCapitalize="none"
                   value={email}
                   onChangeText={setEmail}
-                  outlineColor={error.field === "email" ? Colors.danger : Colors.dark}
+                  outlineColor={
+                    error.field === "email" ? Colors.danger : Colors.dark
+                  }
                   activeOutlineColor={Colors.success}
-                  textColor={error.field === "email" ? Colors.danger : Colors.black}
+                  textColor={
+                    error.field === "email" ? Colors.danger : Colors.black
+                  }
                   style={{ backgroundColor: Colors.white, width: "100%" }}
                   error={error.field === "email"}
                 />
+                <HelperText type="error" visible={error.field === "email"}>
+                  {error.message}
+                </HelperText>
               </View>
             </View>
 
@@ -117,9 +133,13 @@ function LoginScreen() {
                   secureTextEntry={!showPassword}
                   value={password}
                   onChangeText={setPassword}
-                  outlineColor={error.field === "password" ? "#DC0000" : Colors.dark}
+                  outlineColor={
+                    error.field === "password" ? "#DC0000" : Colors.dark
+                  }
                   activeOutlineColor={Colors.success}
-                  textColor={error.field === "password" ? Colors.danger : Colors.black}
+                  textColor={
+                    error.field === "password" ? Colors.danger : Colors.black
+                  }
                   style={{ backgroundColor: Colors.white, width: "100%" }}
                   error={error.field === "password"}
                   right={
@@ -129,6 +149,9 @@ function LoginScreen() {
                     />
                   }
                 />
+                <HelperText type="error" visible={error.field === "password"}>
+                  {error.message}
+                </HelperText>
               </View>
             </View>
 
@@ -148,9 +171,7 @@ function LoginScreen() {
 
             <View style={styles.signUpContainer}>
               <Text style={styles.signUpText}>Don't have account? </Text>
-              <TouchableOpacity
-                onPress={() => navigation.navigate("Signup")}
-              >
+              <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
                 <Text style={styles.signUpLink}>Sign up</Text>
               </TouchableOpacity>
             </View>
@@ -214,12 +235,11 @@ const styles = StyleSheet.create({
 
   // --- Input Styles ---
   inputGroup: {
-    marginBottom: 24,
+    marginBottom: 2,
   },
 
   inputContainer: {
     flexDirection: "column",
-    paddingBottom: 8,
   },
 
   // --- Forgot Password ---
