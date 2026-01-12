1bcaf48f6700cd974ea6022ebc90a878eb21f6eb
Hasibul Hasan <hasibulhasan442@gmail.com>
2026-01-12 22:05:57 +0600

    feat: Integrate internationalization for menu items in AccountSettingOptionScreen

--- src/app/Other/AccountSettingOptionScreen.jsx
+++ src/app/Other/AccountSettingOptionScreen.jsx
@@ -13,25 +13,29 @@ import { CommonStyles } from "../../utils/styles/CommonStyle";
 import { Colors } from "../../utils/constants/Color";
 import MenuItem from "../../utils/helper/MenuItem";
 import { useNavigation } from "@react-navigation/native";
+import { useTranslation } from "react-i18next";
+import { HeaderStyles } from "../../utils/styles/HeaderStyle";
+import HeaderWithBackButton from "../../components/Header/HeaderWithBackButton";
 
 function AccountSettingOptionScreen() {
   const navigation = useNavigation();
+  const { t, i18n } = useTranslation();
   const menuItems = [
     {
       id: 1,
-      title: "Profile Info Update",
+      title: t("menu_names.profile_info_update"),
       icon: <AntDesign name="transaction" size={20} color={Colors.dark} />,
     },
     {
       id: 2,
-      title: "Skills",
+      title: t("menu_names.skills"),
       icon: (
         <Ionicons name="notifications-outline" size={20} color={Colors.dark} />
       ),
     },
     {
       id: 3,
-      title: "Contact Info",
+      title: t("menu_names.contact_information"),
       icon: (
         <Ionicons
           name="shield-checkmark-outline"
@@ -42,14 +46,14 @@ function AccountSettingOptionScreen() {
     },
     {
       id: 4,
-      title: "KYC Verification",
+      title: t("menu_names.kyc_verification"),
       icon: <Ionicons name="language-outline" size={20} color={Colors.dark} />,
       onPress: () => navigation.navigate("OtherPages", { screen: "Kyc" }),
     },
     // { id: 5, title: "Appearance", icon: "color-palette-outline" },
     {
       id: 6,
-      title: "Tax Information",
+      title: t("menu_names.tax_information"),
       icon: (
         <MaterialCommunityIcons
           name="cash-edit"
@@ -60,7 +64,7 @@ function AccountSettingOptionScreen() {
     },
     {
       id: 7,
-      title: "Password & Security",
+      title: t("menu_names.password_and_security"),
       icon: (
         <MaterialCommunityIcons
           name="cash-edit"
@@ -71,7 +75,7 @@ function AccountSettingOptionScreen() {
     },
     {
       id: 8,
-      title: "Withdrawals",
+      title: t("menu_names.withdrawals_method"),
       icon: (
         <MaterialCommunityIcons
           name="cash-edit"
@@ -82,7 +86,7 @@ function AccountSettingOptionScreen() {
     },
     {
       id: 9,
-      title: "Account Deletion",
+      title: t("menu_names.account_deletion"),
       icon: <Fontisto name="money-symbol" size={20} color={Colors.dark} />,
     },
   ];
@@ -91,6 +95,10 @@ function AccountSettingOptionScreen() {
     <>
       <SafeAreaProvider>
         <SafeAreaView style={CommonStyles.safeArea} edges={["top"]}>
+          <View style={HeaderStyles.header}>
+            <HeaderWithBackButton title={t("Pages_Title.accounts")} />
+            <View style={{ width: 40 }} />
+          </View>
           <ScrollView
             style={CommonStyles.container}
             showsVerticalScrollIndicator={false}
