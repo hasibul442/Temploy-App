0584fcbcaa2118a0f2bb921257f28f4e0d355716
Hasibul Hasan <hasibulhasan442@gmail.com>
2026-01-12 22:06:01 +0600

    feat: Implement internationalization for menu items in PreferencesScreen

--- src/app/Other/PreferencesScreen.jsx
+++ src/app/Other/PreferencesScreen.jsx
@@ -8,26 +8,29 @@ import { CommonStyles } from "../../utils/styles/CommonStyle";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Colors } from "../../utils/constants/Color";
 import MenuItem from "../../utils/helper/MenuItem";
+import { useTranslation } from "react-i18next";
+import { HeaderStyles } from "../../utils/styles/HeaderStyle";
+import HeaderWithBackButton from "../../components/Header/HeaderWithBackButton";
 
 function PreferencesScreen() {
   const navigation = useNavigation();
-
+  const { t, i18n } = useTranslation();
   const menuItems = [
     {
       id: 1,
-      title: "Billing & Payment",
+      title: t("menu_names.billing_and_plans"),
       icon: <AntDesign name="transaction" size={20} color={Colors.dark} />,
     },
     {
       id: 2,
-      title: "Notifications",
+      title: t("menu_names.notifications"),
       icon: (
         <Ionicons name="notifications-outline" size={20} color={Colors.dark} />
       ),
     },
     {
       id: 3,
-      title: "Security",
+      title: t("menu_names.security"),
       icon: (
         <Ionicons
           name="shield-checkmark-outline"
@@ -38,14 +41,14 @@ function PreferencesScreen() {
     },
     {
       id: 4,
-      title: "Language",
+      title: t("menu_names.language"),
       icon: <Ionicons name="language-outline" size={20} color={Colors.dark} />,
       onPress: () =>
         navigation.navigate("OtherPages", { screen: "LanguageSelection" }),
     },
     {
       id: 6,
-      title: "Currency",
+      title: t("menu_names.currency"),
       icon: <Fontisto name="money-symbol" size={20} color={Colors.dark} />,
       onPress: () =>
         navigation.navigate("OtherPages", { screen: "CurrencySelection" }),
@@ -55,6 +58,10 @@ function PreferencesScreen() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={CommonStyles.safeArea} edges={["top"]}>
+        <View style={HeaderStyles.header}>
+          <HeaderWithBackButton title={t("Pages_Title.preferences")} />
+          <View style={{ width: 40 }} />
+        </View>
         <ScrollView
           style={CommonStyles.container}
           showsVerticalScrollIndicator={false}
